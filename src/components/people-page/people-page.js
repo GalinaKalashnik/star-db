f8a6db1c6fcc24677ca89ad5e1119e2ba2573292
galaKalashnik <galina.kalashnik@balanceinternet.com.au>
2021-09-24 13:39:48 +0300

    render function передаем разное содержимов в компонент через renderItem function

--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -33,7 +33,8 @@ export default class PeoplePage extends Component {
             <div className="row mb2">
                 <div className="col-md-6">
                     <ItemList onItemSelected={this.onPersonSelected}
-                              getData={this.swapiService.getAllPeople}/>
+                              getData={this.swapiService.getAllPeople}
+                              renderItem={({name, gender, birth_year}) => `${name} (${gender}, ${birth_year})`} />
                 </div>
                 <div className="col-md-6">
                     <PersonDetails personId={this.state.selectedPerson}/>
